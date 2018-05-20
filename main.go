4c244ae646d7c910aa10b7a380c2a242d575fead
zeka <egavrilko@gmail.com>
2018-05-21 01:05:38 +0300

    all methods done

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"image/draw"
 	"image/png"
 
+	"github.com/gonum/matrix/mat64"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/image/tiff"
 )
@@ -47,29 +48,40 @@ func saveImage(img image.Image, path string) error {
 	return Error(png.Encode(fout, img))
 }
 
-func convert(img image.Image) [][]int64 {
-	var numImg [][]int64
+func convert(img image.Image) ([][]int32, [][]int32, [][]int32) {
+	var (
+		redImg   [][]int32
+		greenImg [][]int32
+		blueImg  [][]int32
+	)
 	size := img.Bounds().Max
 
-	numImg = make([][]int64, size.X)
+	redImg = make([][]int32, size.X)
+	greenImg = make([][]int32, size.X)
+	blueImg = make([][]int32, size.X)
 	for i := 0; i < size.X; i++ {
-		numImg[i] = make([]int64, size.Y)
+		redImg[i] = make([]int32, size.Y)
+		greenImg[i] = make([]int32, size.Y)
+		blueImg[i] = make([]int32, size.Y)
 		for j := 0; j < size.Y; j++ {
 			r, g, b, _ := img.At(i, j).RGBA()
 			r /= 257
 			g /= 257
 			b /= 257
-			x := int64((r << 16) + (g << 8) + b) // 24 bits
+			//x := int64((r << 16) + (g << 8) + b) // 24 bits
+			//x := int64(b) // 24 bits
 
-			numImg[i][j] = x
+			redImg[i][j] = int32(r)
+			greenImg[i][j] = int32(g)
+			blueImg[i][j] = int32(b)
 		}
 	}
 
-	return numImg
+	return redImg, greenImg, blueImg
 }
 
-func fragment(img [][]int64, i0, j0, k, s int) [][]int64 {
-	var u = make([][]int64, 2*k+1)
+func fragment(img [][]int32, i0, j0, k, s int) [][]int32 {
+	var u = make([][]int32, 2*k+1)
 
 	for i := 0; i <= k; i++ {
 		u[k+i] = img[i0+i][j0-s : j0+s+1]
@@ -79,8 +91,17 @@ func fragment(img [][]int64, i0, j0, k, s int) [][]int64 {
 	return u
 }
 
-func midPoint(u [][]int64) int64 {
-	var um int64
+func multiFragment(i0, j0, k, s int, img ...[][]int32) [][][]int32 {
+	var ans = make([][][]int32, len(img))
+	for i, img := range img {
+		ans[i] = fragment(img, i0, j0, k, s)
+	}
+
+	return ans
+}
+
+func midPoint(u [][]int32) int32 {
+	var um int32
 
 	for _, arr := range u {
 		for _, a := range arr {
@@ -88,13 +109,25 @@ func midPoint(u [][]int64) int64 {
 		}
 	}
 
-	return um / int64(len(u)*len(u[0]))
+	return um / int32(len(u)*len(u[0]))
 }
 
-func F(u, v [][]int64) float64 {
+func sigma(u [][]int32, midPoint int32) int32 {
+	var um int32
+
+	for _, arr := range u {
+		for _, a := range arr {
+			um += (a - midPoint) * (a - midPoint)
+		}
+	}
+
+	return int32(math.Sqrt(float64(um) / float64(len(u)*len(u[0]))))
+}
+
+func F(u, v [][]int32) float64 {
 	um, vm := midPoint(u), midPoint(v)
 
-	var s1, s2, s3 int64
+	var s1, s2, s3 int32
 
 	for i := range u {
 		for j := range u[i] {
@@ -107,7 +140,7 @@ func F(u, v [][]int64) float64 {
 	return float64(s1) / math.Sqrt(float64(s2)*float64(s3))
 }
 
-func method1(img [][]int64, i0, j0, k, s int) [][]float64 {
+func method1(img [][]int32, i0, j0, k, s int) [][]float64 {
 	var r = make([][]float64, len(img)-k-k)
 
 	u := fragment(img, i0, j0, k, s)
@@ -122,6 +155,204 @@ func method1(img [][]int64, i0, j0, k, s int) [][]float64 {
 	}
 
 	return r
+}
+
+func method2(fi [][][]int32, g [][][]int32) [][]float64 {
+	var (
+		l   = len(fi)
+		mFi = make([]int32, l)
+	)
+	for i := range mFi {
+		mFi[i] = midPoint(fi[i])
+	}
+
+	var data = make([]float64, l*l)
+	var cov = make([][]float64, l)
+
+	for k := range cov {
+		cov[k] = data[k*l : (k+1)*l]
+		for s := range cov {
+			var sum int32
+			for i := range fi {
+				for j := range fi[i] {
+					sum += (fi[k][i][j] - mFi[k]) * (fi[s][i][j] - mFi[s])
+				}
+			}
+
+			cov[k][s] = float64(sum) / float64(len(fi)*len(fi[0]))
+		}
+	}
+
+	ic := mat64.NewDense(l, l, data)
+	for i := 0; i < l; i++ {
+		ic.Set(i, i, ic.At(i, i)+1)
+	}
+	ic.Inverse(ic)
+
+	var r = make([][]float64, len(g[0]))
+
+	for i := range r {
+		r[i] = make([]float64, len(g[0][i]))
+		for j := range r[i] {
+			var y = make([]float64, l)
+
+			for k := range y {
+				y[k] = float64(g[k][i][j] - mFi[k])
+			}
+
+			ans := &mat64.Dense{}
+			ans2 := &mat64.Dense{}
+
+			ans.Mul(mat64.NewDense(1, l, y), ic)
+			ans2.Mul(ans, mat64.NewDense(l, 1, y))
+
+			r[i][j] = ans2.At(0, 0)
+		}
+	}
+
+	return r
+}
+
+func method3(g [][]int32, k int) [][]int32 {
+	m := midPoint(g)
+	s := sigma(g, m)
+
+	l, r := m-2*s, m+2*s
+	kk := int32(1) << uint(k)
+
+	var dg = make([][]int32, len(g))
+	for i := range g {
+		dg[i] = make([]int32, len(g[i]))
+
+		for j, g := range g[i] {
+			if g < l {
+				dg[i][j] = 1
+			} else if g >= r {
+				dg[i][j] = kk
+			} else {
+				dg[i][j] = (g-m+2*s)*(kk-2)/4/s + 2
+			}
+		}
+	}
+
+	return dg
+}
+
+func method4(g [][]int32, k int, s int) ([][]float64, [][]float64, [][]float64) {
+	dg := method3(g, k)
+
+	var (
+		S = make([][]float64, len(dg)-s-s)
+		E = make([][]float64, len(dg)-s-s)
+		W = make([][]float64, len(dg)-s-s)
+	)
+
+	is := 1 / float64((s+1)*(s+1))
+
+	for i := s; i < len(dg)-s; i++ {
+		S[i-s] = make([]float64, len(dg[i])-s-s)
+		E[i-s] = make([]float64, len(dg[i])-s-s)
+		W[i-s] = make([]float64, len(dg[i])-s-s)
+
+		for j := s; j < len(dg[i])-s; j++ {
+			v := fragment(dg, i, j, s, s)
+			var p = make([]float64, 1<<uint(k))
+			for _, t := range v {
+				for _, t := range t {
+					p[t-1]++
+				}
+			}
+			var ss, ee, ww float64
+
+			m := midPoint(v)
+
+			for i := range p {
+				p[i] = p[i] * is
+
+				ss -= p[i] * math.Log2(p[i])
+				ee += p[i] * p[i]
+				ww += float64((i+1-int(m))*(i+1-int(m))) * p[i]
+			}
+
+			S[i-s][j-s] = ss
+			E[i-s][j-s] = ee
+			W[i-s][j-s] = ww
+		}
+	}
+
+	return S, E, W
+}
+
+func method5(g1 [][]int32, g2 [][]int32, k int, s int) ([][]float64, [][]float64, [][]float64) {
+	dg1 := method3(g1, k)
+	dg2 := method3(g2, k)
+
+	var (
+		S = make([][]float64, len(dg1)-s-s)
+		E = make([][]float64, len(dg1)-s-s)
+		W = make([][]float64, len(dg1)-s-s)
+	)
+
+	is := 1 / float64((s+1)*(s+1)*(s+1)*(s+1))
+
+	var p = make([][]float64, 1<<uint(k))
+	for i := range p {
+		p[i] = make([]float64, 1<<uint(k))
+	}
+
+	for i := s; i < len(dg1)-s; i++ {
+		S[i-s] = make([]float64, len(dg1[i])-s-s)
+		E[i-s] = make([]float64, len(dg1[i])-s-s)
+		W[i-s] = make([]float64, len(dg1[i])-s-s)
+
+		for j := s; j < len(dg1[i])-s; j++ {
+			u := fragment(dg1, i, j, s, s)
+			v := fragment(dg2, i, j, s, s)
+
+			for k := range p {
+				for t := range p[k] {
+					p[k][t] = 0
+				}
+			}
+
+			for _, t1 := range u {
+				for _, t1 := range t1 {
+
+					for _, t2 := range v {
+						for _, t2 := range t2 {
+							p[t1-1][t2-1]++
+						}
+					}
+				}
+			}
+
+			var ss, ee, ww float64
+
+			var m1, m2 float64
+
+			for i := range p {
+				for j := range p[i] {
+					p[i][j] = p[i][j] * is
+
+					ss -= p[i][j] * math.Log2(p[i][j])
+					ee += p[i][j] * p[i][j]
+					m1 += float64(i) * p[i][j]
+					m2 += float64(j) * p[i][j]
+				}
+			}
+			for i := range p {
+				for j := range p[i] {
+					ww += (float64(i+1) - m1) * (float64(j+1) - m2) * p[i][j]
+				}
+			}
+
+			S[i-s][j-s] = ss
+			E[i-s][j-s] = ee
+			W[i-s][j-s] = ww
+		}
+	}
+
+	return S, E, W
 }
 
 // HLine draws a horizontal line
@@ -156,13 +387,53 @@ func process() error {
 
 	k, s := 6, 10
 
-	imgNum := convert(img)
-	r := method1(imgNum, 123, 50, k, s)
+	redImg, greenImg, blueImg := convert(img)
 
-	for i := range r {
-		for j := range r[i] {
-			if r[i][j] > 0.65 || r[i][j] < -0.65 {
-				fmt.Println(i, j, r[i][j])
+	dist := method2(multiFragment(123, 50, k, s, redImg, greenImg, blueImg), [][][]int32{redImg, greenImg, blueImg})
+
+	fmt.Println(dist)
+
+	dg := method3(redImg, 4)
+
+	fmt.Println(dg)
+
+	S, E, W := method4(redImg, 4, 6)
+
+	fmt.Println(S)
+	fmt.Println(E)
+	fmt.Println(W)
+
+	S, E, W = method5(redImg, greenImg, 6, s)
+
+	fmt.Println(S)
+	fmt.Println(E)
+	fmt.Println(W)
+
+	rr := method1(redImg, 123, 50, k, s)
+	rg := method1(greenImg, 123, 50, k, s)
+	rb := method1(blueImg, 123, 50, k, s)
+
+	//wr, wg, wb := 20., 70., 40.
+	rk := 0.65
+
+	min := func(a, b, c float64) float64 {
+		if b < a {
+			a = b
+		}
+		if c < a {
+			a = c
+		}
+		return a
+	}
+
+	for i := range rr {
+		for j := range rr[i] {
+			//r := (rr[i][j]*wr + rg[i][j]*wg + rb[i][j]*wb) / (wr + wg + wb)
+			//r := rr[i][j] * rg[i][j] * rb[i][j]
+			r := min(rr[i][j], rg[i][j], rb[i][j])
+
+			if r > rk || r < -rk {
+				fmt.Println(i, j, r)
 
 				Rect(img, i-k, j-s, i+k, j+s)
 			}
